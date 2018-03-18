99d1eae844d1699a6951996a4f52e79b9e11a4bd
Rockvole <rockvole@gmail.com>
2018-03-18 15:18:33 -0700

    Remove ancillary

--- ancillary/tests/globals.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-echo global setup
-#ip=foodaversions.com/iaq
-ip=davidlub/iaq
-
-
--- ancillary/tests/initialize_core.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-source globals.sh
-
-echo ---- All Sensor Group
-curl "http://$ip/initialize_core.php?name=All_Sensor_Group&tz=America/Los_Angeles&core_id_1=TH_Sensor&temp_hum=1&core_id_2=Remaining_Sensors&dust=2&sewer=2&hcho=2&core_id_3=&core_id_4="
-
